ee3a23b649873fa30c39ba0c96d4494bfa25209c
Salem Affes <affessalem@hotmail.fr>
2022-05-17 11:40:43 +0100

    Verify k8s checksum

---
+++ Linux/checksum.sh
@@ -0,0 +1,5 @@
+# https://github.com/kubernetes/kubernetes/releases
+# https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.24.md#downloads-for-v1240
+apt install hashalot
+sha256 kubernetes.tar.gz
+sha512sum kubernetes.tar.gz
