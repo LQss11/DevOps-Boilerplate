d684ea27a8d3d267a439370c4680446b7f68062c
Salem Affes <affessalem@hotmail.fr>
2022-05-24 15:00:35 +0100

    setup docker container runtime

---
+++ Kubernetes/setup-cluster/docker-container-runtime.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+### setting up container runtime prereq
+cat <<- EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Setup required sysctl params, these persist across reboots.
+cat <<- EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+
+# INSTALL DOCKER
+
+sudo apt-get update
+
+sudo apt-get install -y \
+ca-certificates \
+curl \
+gnupg \
+lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg    
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce
+
+sudo mkdir -p /etc/docker
+mkdir -p /etc/systemd/system/docker.service.d
+
+cat > /etc/docker/daemon.json <<- EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+
+
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable docker
