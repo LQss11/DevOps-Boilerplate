e6f79393587a13c88ff9cb1e520c7f12af5a1588
Salem Affes <affessalem@hotmail.fr>
2022-05-26 22:20:18 +0100

    Additional linux commands

--- Linux/commands.sh
+++ Linux/commands.sh
@@ -1,5 +1,5 @@
 find . -name file.txt
-grep -R "my-secret" /var/lib/etcd/
+grep -R "my-secret" .
 less -f -r /var/lib/etcd/member/wal/0000000000000000-0000000000000000.wal
 check | hexdump -C
 echo -n 'mysecret' | base64
@@ -22,3 +22,18 @@ id
 # Get release version
 cat /etc/os-release 
 uname -r 
+# get resources details (top/htop must be enough sometimes)
+df -h 
+free -m
+
+# If ps command not available (get name)
+cat /proc/<process-id>/cmdline
+
+# apps in background
+# PRESS ENTER then run this whenever you wan to get it back in jobs
+kubectl port-forward <pod> 8080:80 &
+jobs
+
+
+
+wget --spider -timeout=1 <ipaddress>
